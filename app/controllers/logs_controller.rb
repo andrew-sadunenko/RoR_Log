ecb5dff394aa794661379868c09306ac10c51048
Andrew <sadunenko88@gmail.com>
2016-11-24 17:34:29 +0200

    formatting logs_controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,17 +1,17 @@
 class LogsController < ApplicationController
-  def create
-  	@log = Log.new(log_params)
-  	@log.ip = Socket::getaddrinfo(Socket.gethostname, "echo", Socket::AF_INET)[0][2] #write ip 
-    @log.created_at = Time.now # write current date/time
-    @log.save # update db
+	def create
+		@log = Log.new(log_params)
+		@log.ip = Socket::getaddrinfo(Socket.gethostname, "echo", Socket::AF_INET)[0][2] #write ip 
+		@log.created_at = Time.now # write current date/time
+		@log.save # update db
 
-    @str = "Hello, " + params[:log][:username] + "!" # write "hello"
+		@str = "Hello, " + params[:log][:username] + "!" # write "hello"
 
-    render "welcome/index"
-  end
+		render "welcome/index"
+	end
 
-  private
-  	def log_params
-  		params.require(:log).permit(:username, :created_at, :ip)
-  	end
+	private
+		def log_params
+			params.require(:log).permit(:username, :created_at, :ip)
+		end
 end
